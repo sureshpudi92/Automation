138d09a8d8c03463d2abd8f84658d266c3a3e2ef
suresh <sureshpudi92@gmail.com>
2024-04-14 11:54:12 +0530

    Webdriver Automation

---
+++ src/test/java/day1/FirstTestCase.java
@@ -0,0 +1,28 @@
+package day1;
+
+/*
+1.open chrome browser
+2.navigate to https://demo.opencart.com/
+3.validate title should be "your store"
+4.close browser
+ */
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTestCase {
+    public static void main(String[] args) {
+
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://demo.opencart.com/");
+        String act_title= driver.getTitle();
+        if(act_title.equals("Your Store")) {
+            System.out.println("Test Passed");
+        }else {
+
+            System.out.println("Test failed");
+        }
+        driver.close();
+    }
+
+}
