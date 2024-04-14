94c503cff827726cf0d621780889ca970f0d230d
suresh <sureshpudi92@gmail.com>
2024-04-14 18:14:38 +0530

    update new file

--- src/test/java/day1/FirstTestCase.java
+++ src/test/java/day1/FirstTestCase.java
@@ -9,6 +9,7 @@ package day1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class FirstTestCase {
     public static void main(String[] args) {
